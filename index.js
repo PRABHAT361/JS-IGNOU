b42e0784444e0d139cc93bab6377a3405e7c0e2c
PRABHAT361 <prabhatprasad54@gmail.com>
2026-02-08 12:32:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-
+// Wait for the page to load
 window.onload = function() {
     const loginForm = document.getElementById('loginForm');
     const messageDiv = document.getElementById('message');
 
     loginForm.onsubmit = function(event) {
-       
+        // Prevent the page from refreshing on submit
         event.preventDefault();
 
-        
+        // Get values from the inputs
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
-        
+        // Check logic
         if (username === "Prabhat" && password === "123") {
             messageDiv.innerHTML = "<h2 style='color: green;'>Login successfully</h2>";
         } else {
